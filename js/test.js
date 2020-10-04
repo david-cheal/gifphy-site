3a46986a7421c82b7f86d81a8292dbee6da063b0
David Cheal <dwryan1996@outlook.com>
2020-10-04 16:32:45 +0100

    Code clean up and consts were not used so implimented them

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 //6dR6cbWxKqlllKecEbptWLwsFj64ilhN
 
-const API_KEY = '6dR6cbWxKqlllKecEbptWLwsFj64ilhN';
-const QUERY_RANDOM_URL = 'https://api.giphy.com/v1/gifs/random';
+const API_KEY = '6dR6cbWxKqlllKecEbptWLwsFj64ilhN'; // Put your API KEY HERE
+const QUERY_RANDOM_URL = 'https://api.giphy.com/v1/gifs/random'; // URL FOR GETTING RANDOM GIF
 
 //Waits for an input change to the text box,
 //on return we scope to the original image url
@@ -9,21 +9,18 @@ const QUERY_RANDOM_URL = 'https://api.giphy.com/v1/gifs/random';
 //then set the url as the source to display image
 $(document).ready(function() {
 
-    var tag = "dog";
+    var tag = "dog"; // Default is dog so the background isn't blank until you update
 
-    var result = $.get("https://api.giphy.com/v1/gifs/random?api_key=6dR6cbWxKqlllKecEbptWLwsFj64ilhN&tag=" + tag + "&rating=g&lang=en", function(response) {
+    var result = $.get(QUERY_RANDOM_URL + "?api_key=" + API_KEY + "&tag=" + tag + "&rating=g&lang=en", function(response) {
 
         result.done(function() {
-            var tag = document.getElementById("search").value;
-
-            var url = response.data.images.original.url
-            document.getElementById("image").style.backgroundImage = "url('" + url + ')";';
+            tag = document.getElementById("search").value;
+            document.getElementById("image").style.backgroundImage = "url('" + response.data.images.original.url + ')";';
         });
         $("input").change(function() {
-            var tag = document.getElementById("search").value;
+            tag = document.getElementById("search").value;
             $.get("https://api.giphy.com/v1/gifs/random?api_key=6dR6cbWxKqlllKecEbptWLwsFj64ilhN&tag=" + tag + "&rating=g&lang=en", function(response) {
-                var url = response.data.images.original.url
-                document.getElementById("image").style.backgroundImage = "url('" + url + ')";';
+                document.getElementById("image").style.backgroundImage = "url('" + response.data.images.original.url + ')";';
             });
         });
     })
